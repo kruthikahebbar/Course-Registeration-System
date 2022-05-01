2de0272550d31b81a0b796c3e0de52362cd70497
kruthikahebbar <93776100+kruthikahebbar@users.noreply.github.com>
2022-05-01 16:36:12 +0530

    Add files via upload

---
+++ DBConnection.java
@@ -0,0 +1,22 @@
+package database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+
+
+
+public class DBConnection {
+    private Connection connection;
+    private String url = "jdbc:mysql://localhost:3306/uiuStudentRecordSystem";
+    private String user = "root";
+    private String pass = "";
+
+    public Connection getConnection() throws SQLException {
+        connection = DriverManager.getConnection(url,user,pass);
+        return connection;
+    }
+
+}
